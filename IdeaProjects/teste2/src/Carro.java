12a0d969ba7b8e0757744d69563ad17c7be8b658
Daniel Pires <dfpires@gmail.com>
2024-02-20 10:38:24 -0300

    Add files via upload

--- IdeaProjects/teste2/src/Carro.java
+++ IdeaProjects/teste2/src/Carro.java
@@ -1,23 +1,45 @@
 public class Carro {
     // Atributos
-    public String marca;
-    public String modelo;
+    public String marca, modelo, cor;
     public int ano;
-    public String cor;
-
-    public Carro(String marca, String modelo, int ano, String cor) {
+    public boolean motor; // true - ligado ou false - desligado
+    public float velAtual;
+    public Carro(String marca, String modelo, int ano, String cor,
+                 boolean motor, float velAtual) {
         this.marca = marca;
         this.modelo = modelo;
         this.ano = ano;
         this.cor = cor;
+        this.motor = motor;
+        this.velAtual = velAtual;
     }
     // Métodos
-    public void acelerar() {
-        System.out.println( this.marca + " Carro acelerando...");
+    public void acelerar(float x) {
+        if (this.motor){
+            this.velAtual = this.velAtual + x;
+        }
     }
 
-    public void frear() {
-        System.out.println( this.modelo + " Carro freando...");
+    public void frear(float x) {
+        if (this.motor){
+            if (this.velAtual - x >= 0){
+                this.velAtual -= x;
+            }
+        }
     }
-
+    // ligar o carro
+    public void ligar(){
+        if (!this.motor)
+            this.motor = true;
+    }
+    public void desligar(){
+        if (this.motor)
+            this.motor = false;
+    }
+    public void mostra(){
+        System.out.println("\n Marca: " + this.marca +
+                " Modelo " + this.modelo + " Ano " + this.ano +
+                " Cor " + this.cor + " Motor " + this.motor +
+                " Velocidade atual " + this.velAtual);
+    }
 }
--- IdeaProjects/teste2/src/Main.java
+++ IdeaProjects/teste2/src/Main.java
@@ -2,12 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
         // Criando objetos da classe Carro
-        Carro meuCarro = new Carro("Toyota", "Corolla", 2022, "Prata");
-        Carro outroCarro = new Carro("Ford", "Fiesta", 2019, "Preto");
+        Carro meuCarro = new Carro("Toyota", "Corolla", 2022, "Prata", true, 0);
+        Carro outroCarro = new Carro("Ford", "Fiesta", 2019, "Preto", false, 0);
 
         // Chamando métodos dos objetos
-        meuCarro.acelerar();
-        outroCarro.frear();
+        //meuCarro.acelerar();
+        //outroCarro.acelerar();
+        //outroCarro.frear();
+        //meuCarro.mostra();
+        //meuCarro.desligar();
+        //meuCarro.mostra();
+        outroCarro.mostra();
+        outroCarro.ligar();
+        outroCarro.acelerar(100);
+        outroCarro.frear(30);
+        outroCarro.mostra();
+
     }
 
 }
